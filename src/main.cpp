2e0a2d425655b4d38b93223caf26827a91629f2f
luckac69 <luckac69@gmail.com>
2026-03-02 15:48:27 -0500

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <GLFW/glfw3.h>
+#include <cstdio>
+
+//#include <glad/glad.h>
+
+int main() {
+    if (!glfwInit())
+        return -1;
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
