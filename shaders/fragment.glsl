aca2c336d64c9881c0f71e038d81dd2f8ccd5faa
luckac69 <luckac69@gmail.com>
2026-03-05 19:55:24 -0500

    multi color triangle

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,9 +1,9 @@
 #version 330 core
 out vec4 FragColor;
   
-uniform vec4 ourColor; // the input variable from the vertex shader (same name and same type)  
+in vec3 ourColor;
 
 void main()
 {
-    FragColor = ourColor;
+    FragColor = vec4(ourColor, 1.0);
 } 
--- src/main.cpp
+++ src/main.cpp
@@ -114,9 +114,9 @@ int main() {
 	float firstTriangle[] = {
 
 
-		-0.45f, 0.5f, 0.0f,
-		-0.9f, -0.5f, 0.0f,
-		0.0f, -0.5f, 0.0f
+		-0.45f, 0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
+		-0.9f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
+		0.0f, -0.5f, 0.0f,   0.0f, 0.0f, 1.1f
 	};
 	float secondTriangle[] = {
 		0.0f, 0.5f, 0.0f,
@@ -138,8 +138,10 @@ int main() {
 	glBindVertexArray(VAOs[0]);
 	glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangle), firstTriangle, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	// triangle 2
 	glBindVertexArray(VAOs[1]);
